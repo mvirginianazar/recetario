701bf73ffa890384d118d195fee6fdc5714c5bae
Virginia <mvirginianazar@gmail.com>
2020-12-21 20:37:47 +1300

    muchos cambios
    cambio de fonts + agregado navidad + comienzo de codigo con js + etc

---
+++ index.js
@@ -0,0 +1,28 @@
+// document lo define el browser y no lo tocamos
+
+// window que lo define el browser pero podemos agregar cosas
+
+let config = { 
+    guisoDeLenteja: { 
+        title: "Guiso de Lentejas",
+        procedimiento: [
+            "En una cacerola rehogar la cebolla, el ajo y la zanahoria en aceite. Agregar el morrón cortado en pequeños trozos.cortar y preparar vegetales.En una cacerola rehogar la cebolla, el ajo y la zanahoria en aceite. Agregar el morrón cortado en pequeños trozos.cortar y preparar vegetales",
+            "cocinarIncorporar la carne al rehogado. Condimentar con pimienta y las hierbas aromáticas. Verter el vino y dejar evaporar el alcohol."
+        ]
+    },
+    shortRibs: { 
+        title: "'Slow cooked short ribs",
+        procedimiento: [
+            "STEP 1",
+            "STEP 2"
+        ]
+    }
+}
+
+function onReceipeClicked(configId) {
+    const selectedReceipe = config[configId];
+    console.log(selectedReceipe);
+    localStorage.setItem('vicky', JSON.stringify(selectedReceipe) );
+    window.location.href = "/receta-detalle-template.html"
+}
+
---
+++ recetas.js
@@ -0,0 +1,10 @@
+// extraigo la configuracion
+const configReceta = JSON.parse(localStorage.getItem('vicky'));
+
+console.log(configReceta);
+//uso el valor y asigno el titulo
+const elementTitulo = document.getElementById("receta-titulo");
+// elementTitulo.innerText = "titulo provisorio"
+elementTitulo.innerText = configReceta.title;
+
+
